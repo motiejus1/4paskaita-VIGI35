3da10d813a15d86b3a7748395d7ee9b27dbdb97b
motiejus1 <motiejus.baliukonis@vka.lt>
2022-09-19 19:48:52 +0300

    *pridejau skaiciavimo forma

--- index.php
+++ index.php
@@ -1,4 +1,23 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>4paskaita</title>
+</head>
+<body>
+    <form method="get" action="index.php">
+        <input name="skaitiklis1">
+        <input name="vardiklis1">
+        <input name="skaitiklis2">
+        <input name="vardiklis2">
+        <button type="submit">Sudeti</button>
+    </form>
+</body>
+</html>
+
 <?php 
 //GitHub
-//_SERVER
 // 2 namu darba
